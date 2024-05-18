511be888b97e239611f2cc382951f304c4ab7744
Manjit Bedi <dev@manjitbedi.com>
2024-05-17 21:03:39 -0700

    adding throwing & sound effects

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -2,22 +2,41 @@ using UnityEngine;
 
 /// <summary>
 /// Class to drag & drop (release) the selected game object using the old input system.
-/// When the object is released, play an animation sequence.
+/// Handle when the user grabs a carrot & throws it.
+/// If the mouse movement is not too large, just propel the carrot up.
+/// Full discloure I used some code form here to save on development time:
+/// https://pastebin.com/2JPEYMiw
 /// </summary>
 
 // TODO: implement this using the newer input system
 
 public class DragAndDrop : MonoBehaviour
 {
-    Vector3 mousePosition;
+     Vector3 mousePosition;
 
-    CarrotGameObject carrotGameObject;
+     CarrotGameObject carrotGameObject;
 
-    public GameManager gameManager;
+     public GameManager gameManager;
+
+     float startTime, endTime, swipeDistance, swipeTime;
+     private Vector2 startPos;
+     private Vector2 endPos;
+
+     public float MinSwipDist = 0;
+     private float objectVelocity = 0;
+     private float objectSpeed = 0;
+     public float maxObjectSpeed = 350;
+     private Vector3 angle;
+
+     private bool thrown, holding;
+     private Vector3 newPosition, resetPos;
+     Rigidbody rb;
 
     private void Start()
     {
           carrotGameObject = gameObject.GetComponent<CarrotGameObject>();
+
+          rb = GetComponent<Rigidbody>();
     }
 
     private Vector3 GetMousePos() 
@@ -33,6 +52,9 @@ public class DragAndDrop : MonoBehaviour
           mousePosition = Input.mousePosition - GetMousePos();
           gameManager.selectedCarrot = carrotGameObject;
           carrotGameObject.Grabbed();
+
+          startTime = Time.time;
+          startPos = Input.mousePosition;
      }
 
 
@@ -41,13 +63,58 @@ public class DragAndDrop : MonoBehaviour
      {
           // Need to adjust the mouse position to use in world space.
           transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
-          
      }
 
      // The carrot has been released, play an animation.
      private void  OnMouseUp() 
      {          
-          carrotGameObject.AnimateRemove();
+          endTime = Time.time;
+          endPos = Input.mousePosition;
+          swipeDistance = (endPos - startPos).magnitude;
+          swipeTime = endTime - startTime;
+
+          if (swipeTime < 0.5f && swipeDistance > 30f)
+          {
+               //throw the game object
+               CalSpeed();
+               CalAngle();
+               rb.AddForce(new Vector3((angle.x * objectSpeed), (angle.y * objectSpeed / 3), (angle.z * objectSpeed) * 2));
+               rb.useGravity = true;
+               holding = false;
+               thrown = true;
+               gameManager.PlayAudio(GameAudio.Throw); 
+               Invoke("RemoveObject", 4f);
+          }
+          else
+          {
+               carrotGameObject.AnimateRemove();
+          }
      }
+
+     private void CalAngle()
+     {
+          angle = Camera.main.ScreenToWorldPoint(new Vector3(endPos.x, endPos.y + 50f, (Camera.main.nearClipPlane + 5)));
+     }
+
+    void CalSpeed()
+    {
+        if (swipeTime > 0)
+        {
+            objectVelocity = swipeDistance / (swipeDistance - swipeTime);
+        }
+
+        objectSpeed = objectVelocity * 40;
+
+        if (objectSpeed <= maxObjectSpeed)
+        {
+            objectSpeed = maxObjectSpeed;
+        }
+        swipeTime = 0;
+    }
+
+    void RemoveObject() 
+    {
+          gameManager.RemoveCarrotFromScene(carrotGameObject);
+    }
 }
 
